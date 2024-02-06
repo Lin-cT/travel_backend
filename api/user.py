5374116e04ad87af2fb566b6847df794aa95052f
lin-ct <lindsaytang8@gmail.com>
2024-02-06 14:31:33 -0800

    FIXED

--- api/user.py
+++ api/user.py
@@ -1,9 +1,8 @@
 import json, jwt
-from flask import Blueprint, request, jsonify, current_app, Response
+from flask import Flask, Blueprint, request, jsonify, current_app, Response, make_response
 from flask_restful import Api, Resource # used for REST API building
 from datetime import datetime
 from auth_middleware import token_required
-
 from model.users import User
 
 user_api = Blueprint('user_api', __name__,
@@ -14,58 +13,46 @@ api = Api(user_api)
 
 class UserAPI:        
     class _CRUD(Resource):  # User API operation for Create, Read.  THe Update, Delete methods need to be implemeented
-
+        #@token_required
         def post(self): # Create method
             ''' Read data for json body '''
             body = request.get_json()
-
+            print(body)
             ''' Avoid garbage in, error checking '''
             # validate name
             name = body.get('name')
-
             if name is None or len(name) < 2:
-                return {'message': f'Name is missing, or is less than 2 characters'}, 400
-
+                return {'message': f'Name is missing, or is less than 2     acters'}, 400
             # validate uid
             uid = body.get('uid')
             if uid is None or len(uid) < 2:
                 return {'message': f'User ID is missing, or is less than 2 characters'}, 400
             # look for password and dob
             password = body.get('password')
-            dob = body.get('dob')
-
             ''' #1: Key code block, setup USER OBJECT '''
-            uo = User(name=name, 
-                      uid=uid)
+            uo = User(name=name, uid=uid)
             
             ''' Additional garbage error checking '''
             # set password if provided
             if password is not None:
                 uo.set_password(password)
             # convert to date type
-            if dob is not None:
-                try:
-                    uo.dob = datetime.strptime(dob, '%Y-%m-%d').date()
-                except:
-                    return {'message': f'Date of birth format error {dob}, must be yyyy-mm-dd'}, 400
-            
-            ''' #2: Key Code block to add user to database '''
-
             # create user in database
             user = uo.create()
             # success returns json of user
             if user:
-                return jsonify(user.read())
+                print(user.read())
+                return ((user.read()), 200)
             # failure returns error
-            return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 400
+            return make_response({'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 400)
 
-        @token_required(roles=["Admin"])
+        @token_required
         def get(self, current_user): # Read Method
             users = User.query.all()    # read/extract all users from database
             json_ready = [user.read() for user in users]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
-    
-        @token_required(roles=["Admin"])
+        
+        @token_required
         def delete(self, current_user):
             body = request.get_json()
             uid = body.get('uid')
@@ -75,23 +62,22 @@ class UserAPI:
                     user.delete()
             return jsonify(user.read())
         
-        @token_required(roles=["Admin"])
+        @token_required
         def put(self, current_user):
             body = request.get_json() # get the body of the request
             uid = body.get('uid') # get the UID (Know what to reference)
-            name = body.get('name') # get name (to change)
-            password = body.get('password') # get password (to change)
-            role = body.get('role')  # get role (to change)
-            users = User.query.all() # get users
+            name = body.get('name')
+            users = User.query.all()
             for user in users:
-                if user.uid == uid: # find user with matching uid
-                    user.update(name,'',password, role) # update info
+                if user.uid == uid:
+                    user.update(name,'','')
             return f"{user.read()} Updated"
-        
+    
     class _Security(Resource):
         def post(self):
             try:
                 body = request.get_json()
+                print(body)
                 if not body:
                     return {
                         "message": "Please provide user details",
@@ -100,42 +86,32 @@ class UserAPI:
                     }, 400
                 ''' Get Data '''
                 uid = body.get('uid')
-
                 if uid is None:
+                    print("error at uid")
                     return {'message': f'User ID is missing'}, 400
                 password = body.get('password')
-
+                
                 ''' Find user '''
                 user = User.query.filter_by(_uid=uid).first()
                 if user is None or not user.is_password(password):
+                    print("error at password")
                     return {'message': f"Invalid user id or password"}, 400
-
                 if user:
                     try:
-                        token_payload = {
-                            "_uid": user._uid,
-                            "role": user._role,
-                        }
-                        print(f"token_payload={token_payload}")
                         token = jwt.encode(
-                            token_payload,
+                            {"_uid": user._uid},
                             current_app.config["SECRET_KEY"],
                             algorithm="HS256"
                         )
                         resp = Response("Authentication for %s successful" % (user._uid))
-                        resp.set_cookie("jwt", token,
-                                max_age=3600,
-                                secure=True,
-                                httponly=True,
-                                path='/',
-                                samesite='None',
-                                )
+                        resp.set_cookie(key="jwt", value=token, max_age=3600, secure=False, samesite='None', path='/', httponly=False)
+                        print(resp.headers)
                         return resp
                     except Exception as e:
                         return {
                             "error": "Something went wrong",
                             "message": str(e)
-                        }, 501
+                        }, 500
                 return {
                     "message": "Error fetching auth token!",
                     "data": None,
@@ -146,32 +122,10 @@ class UserAPI:
                         "message": "Something went wrong!",
                         "error": str(e),
                         "data": None
-                }, 502
-    
-    class _Create(Resource):
-        def post(self):
-            body = request.get_json()
-            # Fetch data from the form
-            name = body.get('name')
-            uid = body.get('uid')
-            password = body.get('password')
-            dob = body.get('dob')
-            animal = body.get('animal')
-            role = body.get('role')
-            if animal is not None:
-                new_user = User(name=name, uid=uid, password=password, dob=dob, animal=animal, role=role)
-            else:
-                new_user = User(name=name, uid=uid, password=password, dob=dob, animal='', role=role)
-            user = new_user.create()
-            # success returns json of user
-            if user:
-                #return jsonify(user.read())
-                return user.read()
-            # failure returns error
-            return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 400
+                }, 500
 
+            
     # building RESTapi endpoint
     api.add_resource(_CRUD, '/')
     api.add_resource(_Security, '/authenticate')
-    api.add_resource(_Create, '/create')
     
--- auth_middleware.py
+++ auth_middleware.py
@@ -4,45 +4,33 @@ from flask import request, abort
 from flask import current_app
 from model.users import User
 
-def token_required(roles=None):
-    def decorator(f):
-        @wraps(f)
-        def decorated(*args, **kwargs):
-            
-            token = request.cookies.get("jwt")
-
-            if not token:
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.cookies.get("jwt")
+        if not token:
+            return {
+                "message": "Authentication Token is missing!",
+                "data": None,
+                "error": "Unauthorized",
+            }, 403
+        try:
+            data=jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
+            current_user=User.query.filter_by(_uid=data["_uid"]).first()
+            print(current_user.role)
+            if current_user is None or current_user.role != "Admin":
                 return {
-                    "message": "Authentication Token is missing!",
-                    "data": None,
-                    "error": "Unauthorized"
-                }, 401
-            try:
-                data = jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
-                current_user = User.query.filter_by(_uid=data["_uid"]).first()
-                if current_user is None:
-                    return {
-                        "message": "Invalid Authentication token!",
-                        "data": None,
-                        "error": "Unauthorized"
-                    }, 401
+                "message": "No authorization.",
+                "data": None,
+                "error": "Unauthorized"
+            }, 401
+        except Exception as e:
+            return {
+                "message": "Something went wrong",
+                "data": None,
+                "error": str(e)
+            }, 500
 
-                if roles and current_user.role not in roles:
-                    return {
-                        "message": "Insufficient permissions. Required roles: {}".format(roles),
-                        "data": None,
-                        "error": "Forbidden"
-                    }, 403
+        return f(current_user, *args, **kwargs)
 
-            except Exception as e:
-
-                return {
-                    "message": "Something went wrong",
-                    "data": None,
-                    "error": str(e)
-                }, 500
-
-            return f(current_user, *args, **kwargs)
-
-        return decorated
-    return decorator
+    return decorated
