a2ce652eba44c5ac019ac638a78917a765196c75
lin-ct <lindsaytang8@gmail.com>
2024-02-13 15:03:16 -0800

    changing users for our project

--- model/users.py
+++ model/users.py
@@ -16,15 +16,15 @@ class User(db.Model):
     _uid = db.Column(db.String(255), unique=True, nullable=False)
     _password = db.Column(db.String(255), unique=False, nullable=False)
     _role = db.Column(db.String(255), unique=False)
-    _animal = db.Column(db.String(255))
+    _itinerary = db.Column(db.String(255))
     # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
     # constructor of a User object, initializes the instance variables within object (self)
-    def __init__(self, name, uid, password="123qwerty", role="User", animal="cat"):
+    def __init__(self, name, uid, password="123qwerty", role="User", itinerary='Mondsay: Swim'):
         self._name = name    # variables with self prefix become part of the object,
         self._uid = uid
         self.set_password(password)
         self._role = role
-        self._animal = animal
+        self._itinerary = itinerary
     # a name getter method, extracts name from object
     @property
     def name(self):
@@ -63,11 +63,11 @@ class User(db.Model):
     def role(self, role):
         self._role = role
     @property
-    def animal(self):
-        return self._animal
-    @animal.setter
-    def animal(self, animal):
-        self._animal = animal
+    def itinerary(self):
+        return self._itinerary
+    @itinerary.setter
+    def itinerary(self, itinerary):
+        self._role = itinerary
     # dob property is returned as string, to avoid unfriendly outcomes
     # output content using str(object) in human readable form, uses getter
     # output content using json dumps, this is ready for API response
@@ -92,11 +92,11 @@ class User(db.Model):
             "name": self.name,
             "uid": self.uid,
             "role": self.role,
-            "animal": self.animal
+            "itinerary": self.itinerary
         }
     # CRUD update: updates user name, password, phone
     # returns self
-    def update(self, name="", uid="", password="", role="User", animal=""):
+    def update(self, name="", uid="", password="", role="User", itinerary=""):
         """only updates values with length"""
         if len(name) > 0:
             self.name = name
@@ -106,8 +106,7 @@ class User(db.Model):
             self.set_password(password)
         if len(role) > 0:
             self.role = role
-        if len(animal) > 0:
-            self.animal = animal
+        self.itinerary = itinerary
         db.session.commit()
         return self
     # CRUD delete: remove self
@@ -123,12 +122,12 @@ def initUsers():
         """Create database and tables"""
         db.create_all()
         """Tester data for table"""
-        u1 = User(name='Thomas Edison', uid='toby', password='123toby', animal="tiger")
-        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko', animal="rabbit")
-        u3 = User(name='Alexander Graham Bell', uid='lex', animal="pig")
-        u4 = User(name='Grace Hopper', uid='hop', password='123hop', animal="ox")
+        u1 = User(name='Thomas Edison', uid='toby', password='123toby')
+        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko')
+        u3 = User(name='Alexander Graham Bell', uid='lex')
+        u4 = User(name='Grace Hopper', uid='hop', password='123hop')
         u5 = User(name='Lindsay Tang', uid='lct', password='123lin', role="Admin")
-        u6 = User(name='Ben Chin', uid='BenC', password='123ben', role="Admin", animal="dog")
+        u6 = User(name='Ben Chin', uid='BenC', password='123ben', role="Admin")
         users = [u1, u2, u3, u4, u5, u6]
         """Builds sample user/note(s) data"""
         for user in users:
